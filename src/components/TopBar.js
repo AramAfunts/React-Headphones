ef331dddda5139f3d0456e8981efb33cb13d1f59
AramAfunts <afuncaram354@gmail.com>
2023-03-17 22:02:18 +0400

    created project architecture + top-bar

---
+++ src/components/TopBar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const TopBar = () => {
+  return (
+    <div className="top-bar flex px-10 py-1">
+      <div className="block">
+        <p className="text-white">+001234567890</p>
+      </div>
+      <div className="block">
+        <p className="text-white">Get 50% Off on Selected Items | Shop Now</p>
+      </div>
+      <div className="flex">
+        <img src="img/uk-icon.png" alt="eng" width={25} />
+        <p className="text-white ml-2">ENG</p>
+      </div>
+    </div>
+  );
+};
